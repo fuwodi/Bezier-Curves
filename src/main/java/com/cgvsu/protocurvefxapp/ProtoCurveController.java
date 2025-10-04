2a02c567f4f44fc84bcfe72bf82eb5e377662bb2
fuwodi <kondratyeva2006@yandex.ru>
2025-10-04 18:09:09 +0300

    Refactor point and polyline rendering methods

--- src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
+++ src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
@@ -6,8 +6,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProtoCurveController {
 
@@ -17,31 +19,61 @@ public class ProtoCurveController {
     private Canvas canvas;
 
     ArrayList<Point2D> points = new ArrayList<Point2D>();
+    final int POINT_RADIUS = 3;
 
     @FXML
     private void initialize() {
-        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
-        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
+        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> {
+            canvas.setWidth(newValue.doubleValue());
+            redrawAll();
+        });
+        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> {
+            canvas.setHeight(newValue.doubleValue());
+            redrawAll();
+        });
 
         canvas.setOnMouseClicked(event -> {
             switch (event.getButton()) {
                 case PRIMARY -> handlePrimaryClick(canvas.getGraphicsContext2D(), event);
+                case SECONDARY -> clear(canvas.getGraphicsContext2D());
             }
         });
     }
 
+    private void clear(GraphicsContext graphicsContext){
+        points.clear();
+        redrawAll();
+    }
+
+    private void redrawAll(){
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        drawPointsAndLine(graphicsContext);
+    }
+
     private void handlePrimaryClick(GraphicsContext graphicsContext, MouseEvent event) {
         final Point2D clickPoint = new Point2D(event.getX(), event.getY());
+        points.add(clickPoint);
+        redrawAll();
+    }
 
-        final int POINT_RADIUS = 3;
-        graphicsContext.fillOval(
-                clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS,
-                2 * POINT_RADIUS, 2 * POINT_RADIUS);
+    private void drawPointsAndLine(GraphicsContext graphicsContext){
+        for (Point2D point : points) {
+            graphicsContext.fillOval(
+                    point.getX() - POINT_RADIUS, point.getY() - POINT_RADIUS,
+                    2 * POINT_RADIUS, 2 * POINT_RADIUS);
+        }
 
         if (points.size() > 0) {
-            final Point2D lastPoint = points.get(points.size() - 1);
-            graphicsContext.strokeLine(lastPoint.getX(), lastPoint.getY(), clickPoint.getX(), clickPoint.getY());
+            graphicsContext.setStroke(Color.BLACK);
+            for (int i = 0; i< points.size()-1; i++){
+                Point2D curr = points.get(i);
+                Point2D next = points.get(i+1);
+                graphicsContext.strokeLine(curr.getX(), curr.getY(), next.getX(), next.getY());
+            }
         }
-        points.add(clickPoint);
     }
+
+
 }
